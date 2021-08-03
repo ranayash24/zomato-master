ce18d12bebaec5b5bd95533015e61258d0cd05ca
ranayash24 <homehelp24022000@gmail.com>
2021-08-03 22:28:46 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+// Importing Env Variables
+require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import passport from "passport";
+
+// configs
+import googleAuthConfig from "./config/google.config";
+
+// microservice routes
+import Auth from "./API/Auth";
+import Restaurant from "./API/Restaurant";
+import Food from "./API/Food";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+const zomato = express();
+
+// application middlewares
+zomato.use(express.json());
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+// passport cofiguration
+googleAuthConfig(passport);
+
+// Application Routes
+zomato.use("/auth", Auth);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/food", Food);
+
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
+
+zomato.listen(4001, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
